af1e320bb4476d86aa8944ddb87c7fd59040ab18
Na5tyGoose <letyxxxa@gmail.com>
2023-04-30 20:19:00 +0200

    Split into functions, use object instead of array, fix behavior when second word was transformed into original

--- src/EditDistance.ts
+++ src/EditDistance.ts
@@ -1,98 +1,133 @@
-const minimalDistance = (word1: string, word2: string): number => {
-  console.log(word1, word2);
+const getActionCost = (i: number, j: number, dpMatrix: number[][]): number => {
+  if (i < 0 && j < 0) return 0;
+  if (i < 0) return j + 1;
+  if (j < 0) return i + 1;
+  return dpMatrix[i][j];
+};
 
-  const firstWordLength = word1.length;
-  const secondWordLength = word2.length;
+const buildWord = (
+  matrix: number[][],
+  stepsNumToPerform: number,
+  words: {
+    originalWord: string;
+    targetWord: string;
+  }
+): string => {
+  const { targetWord, originalWord } = words;
+
+  let curI = targetWord.length - 1;
+  let curJ = originalWord.length - 1;
+
+  // Use object instead of array because indexes may be negative integers
+  let curWordObject = Array.from(originalWord).reduce((acc, char, index) => {
+    acc[index] = char;
+
+    return acc;
+  }, {});
+
+  // Build word using DP matrix
+  while (stepsNumToPerform > 0) {
+    // Delete operation cost in matrix will always be at the top of current cursor position
+    const deleteActionCost = getActionCost(curI, curJ - 1, matrix);
+    // Insert operation cost in matrix will always be at the left of current cursor position
+    const insertActionCost = getActionCost(curI - 1, curJ, matrix);
+    // Replace operation cost in matrix will always be at the top left of current cursor position
+    const replaceActionCost = getActionCost(curI - 1, curJ - 1, matrix);
+
+    if (replaceActionCost < stepsNumToPerform) {
+      console.log(curJ);
+
+      curWordObject[curJ] = targetWord[curI];
+
+      // Put cursor to new position in matrix
+      curI -= 1;
+      curJ -= 1;
+
+      stepsNumToPerform = replaceActionCost;
+      console.log("Replace operation");
+    } else if (deleteActionCost < stepsNumToPerform) {
+      curWordObject[curJ] = "";
+
+      // Put cursor to new position in matrix
+      curJ -= 1;
+
+      stepsNumToPerform = deleteActionCost;
+      console.log("Delete operation");
+    } else if (insertActionCost < stepsNumToPerform) {
+      curWordObject[curJ + 1] = targetWord[curI];
+
+      // Put cursor to new position in matrix
+      curI -= 1;
+
+      stepsNumToPerform = insertActionCost;
+      console.log("Insert operation");
+    } else {
+      // If no action with lower cost that current steps number is found move cursor to the top left in matrix
+      // and repeat the flow
+      curI -= 1;
+      curJ -= 1;
+    }
+  }
+
+  const finalWord = Object.keys(curWordObject)
+    // Sort keys as they are indexes of the final word
+    .sort((a, b) => Number(a) - Number(b))
+    // Map indexes to actual chars
+    .map((key) => curWordObject[key])
+    // Join into single word
+    .join("");
+
+  return finalWord;
+};
+
+const minimalDistance = (
+  inputWord1: string,
+  inputWord2: string
+): [number, string] => {
+  const originalWordLength = inputWord1.length;
+  const targetWordLength = inputWord2.length;
 
   // Initializing empty array with length equal to first word length
-  const firstWordArray = Array(firstWordLength);
-
-  const insertIntoArray = (arr: string[], index: number, newItem: string) => [
-    // part of the array before the specified index
-    ...arr.slice(0, index),
-    // inserted item
-    newItem,
-    // part of the array after the specified index
-    ...arr.slice(index),
-  ];
-
-  const getActionCost = (i: number, j: number, firstWordArray: number[]) => {
-    if (i < 0 && j < 0) return 0;
-    if (i < 0) return j + 1;
-    if (j < 0) return i + 1;
-    return firstWordArray[i][j];
-  };
+  const dpMatrix = Array(targetWordLength);
 
   // Initializng DP matrix, calcualting and filling it with values
   // Columns are standing for the first word letters
-  for (let i = 0; i < firstWordLength; i++) {
-    firstWordArray[i] = Array(secondWordLength);
+  for (let i = 0; i < targetWordLength; i++) {
+    dpMatrix[i] = Array(originalWordLength);
+
     // Rows are standing for the second word letters
-    for (let j = 0; j < secondWordLength; j++) {
+    for (let j = 0; j < originalWordLength; j++) {
       // Filling the DP matrix with values
-      firstWordArray[i][j] = Math.min(
+      dpMatrix[i][j] = Math.min(
         // Caculate insert action cost
-        getActionCost(i - 1, j, firstWordArray) + 1,
+        getActionCost(i - 1, j, dpMatrix) + 1,
         // Caculate delete action cost
-        getActionCost(i, j - 1, firstWordArray) + 1,
+        getActionCost(i, j - 1, dpMatrix) + 1,
         // Caculate replacement action cost
-        getActionCost(i - 1, j - 1, firstWordArray) +
-          (word1[i] === word2[j] ? 0 : 1)
+        getActionCost(i - 1, j - 1, dpMatrix) +
+          (inputWord1[j] === inputWord2[i] ? 0 : 1)
       );
     }
   }
 
-  let stepsToPerform = getActionCost(
-    firstWordLength - 1,
-    secondWordLength - 1,
-    firstWordArray
+  const resultDistance = getActionCost(
+    targetWordLength - 1,
+    originalWordLength - 1,
+    dpMatrix
   );
 
-  // Save the result distance into const to return it
-  const resultDistance = stepsToPerform;
+  // As this functionality is not requested in original task definition but was already implented
+  // it was utilized to separate function
+  const rebuildedWord = buildWord(dpMatrix, resultDistance, {
+    originalWord: inputWord1,
+    targetWord: inputWord2,
+  });
 
-  console.log("Steps to perform: ", stepsToPerform);
+  console.log("\n");
+  console.log("Result word: ", rebuildedWord);
+  console.log("Result distance: ", resultDistance);
 
-  let curI = firstWordLength - 1;
-  let curJ = secondWordLength - 1;
-  let curWord = Array.from(word2);
-
-  while (stepsToPerform > 0) {
-    const deleteActionCost = getActionCost(curI, curJ - 1, firstWordArray);
-    const insertActionCost = getActionCost(curI - 1, curJ, firstWordArray);
-    const replaceActionCost = getActionCost(curI - 1, curJ - 1, firstWordArray);
-
-    if (replaceActionCost < stepsToPerform) {
-      console.log("Replace operation");
-
-      curWord[curJ] = word1[curI];
-      curI -= 1;
-      curJ -= 1;
-      stepsToPerform = replaceActionCost;
-      console.log(curWord.join(""));
-    } else if (deleteActionCost < stepsToPerform) {
-      console.log("Delete operation");
-
-      curWord[curJ] = "";
-      curJ -= 1;
-      stepsToPerform = deleteActionCost;
-      console.log(curWord.join(""));
-    } else if (insertActionCost < stepsToPerform) {
-      console.log("Insert operation");
-
-      curWord = insertIntoArray(curWord, curJ + 1, word1[curI]);
-      curI -= 1;
-      stepsToPerform = insertActionCost;
-      console.log(curWord.join(""));
-    } else {
-      curI -= 1;
-      curJ -= 1;
-    }
-  }
-
-  console.log("Final word: ", curWord, curWord[-1]);
-
-  return resultDistance;
+  return [resultDistance, rebuildedWord];
 };
 
 export default minimalDistance;
--- src/__tests__/EditDistance.test.ts
+++ src/__tests__/EditDistance.test.ts
@@ -1,38 +1,62 @@
 import minimalDistance from "../EditDistance";
 
 describe("Edit distance", () => {
-  it("when first word has more symbols than second", async () => {
+  it("when first word has more symbols than second should correctly calculate result", async () => {
     const firstWord = "test_word";
     const secondWord = "word";
 
-    const distance = minimalDistance(firstWord, secondWord);
+    const [distance, word] = minimalDistance(firstWord, secondWord);
 
-    expect(distance).toEqual(5)
+    expect(distance).toEqual(5);
+    expect(word).toEqual(secondWord);
   });
 
-  it("when first word has less symbols than second", async () => {
+  it("when first word has less symbols than second should correctly calculate result", async () => {
     const firstWord = "word";
     const secondWord = "test_word";
-    const distance = minimalDistance(firstWord, secondWord);
+    const [distance, word] = minimalDistance(firstWord, secondWord);
 
-    expect(distance).toEqual(5)
+    expect(distance).toEqual(5);
+    expect(word).toEqual(secondWord);
   });
 
-  it("when first word has equal number of symbols than second", async () => {
+  it("when first word has equal number of symbols than second should correctly calculate result", async () => {
     const firstWord = "test_word_1";
     const secondWord = "test_word_2";
 
-    const distance = minimalDistance(firstWord, secondWord);
+    const [distance, word] = minimalDistance(firstWord, secondWord);
 
-    expect(distance).toEqual(1)
+    expect(distance).toEqual(1);
+    expect(word).toEqual(secondWord);
   });
 
-  it("when first word is identical to second", async () => {
+  it("when first word is identical to second should correctly calculate result", async () => {
     const firstWord = "test_word";
     const secondWord = "test_word";
 
-    const distance = minimalDistance(firstWord, secondWord);
+    const [distance, word] = minimalDistance(firstWord, secondWord);
 
-    expect(distance).toEqual(0)
+    expect(distance).toEqual(0);
+    expect(word).toEqual(secondWord);
+  });
+
+  it("when first word is identical to second should correctly calculate result", async () => {
+    const firstWord = "test_word";
+    const secondWord = "test_word";
+
+    const [distance, word] = minimalDistance(firstWord, secondWord);
+
+    expect(distance).toEqual(0);
+    expect(word).toEqual(secondWord);
+  });
+
+  it("when first word has space inside should correctly calculate result", async () => {
+    const firstWord = "test _ word";
+    const secondWord = "test_word";
+
+    const [distance, word] = minimalDistance(firstWord, secondWord);
+
+    expect(distance).toEqual(2);
+    expect(word).toEqual(secondWord);
   });
 });
